2e579963247edd717142e6b315d242921f5bf44b
Emanuel Alvarez <35038176+ema6460@users.noreply.github.com>
2019-01-06 23:28:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+class Product {
+
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI {
+    addProduct() {
+
+    }
+
+    deleteProduct() {
+
+    }
+
+    showMessage() {
+
+    }
+}
+
+// DOM EVENTS
+document.getElementById('product-form')
+    .addEventListener('submit', function (e) {
+
+        const name = document.getElementById('name').value;
+        const price = document.getElementById('price').value;
+        const year = document.getElementById('year').value;
+
+        console.log(name, price, year);
+
+        const product = new Product(name, price, year);
+
+        e.preventDefault();
+    });
